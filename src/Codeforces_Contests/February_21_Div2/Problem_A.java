95cb89ab7128235b1569e86db6414a0ac31962f4
Sumit Kumar <developer.sumit.professional@gmail.com>
2026-02-21 19:25:42 +0530

    Questions added

---
+++ src/Codeforces_Contests/February_21_Div2/Problem_A.java
@@ -0,0 +1,13 @@
+package Codeforces_Contests.February_21_Div2;
+
+import java.util.*;
+public class Problem_A {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        while(t-->0){
+
+        }
+    }
+}
